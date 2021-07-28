dd00155691a277f001c0f86d579f126a6303d7de
Nico Rithner <nico@altvia.com>
2021-07-28 07:42:42 -0600

    Declare multiple states

--- 2-set-and-update-state-with-useState/state-hook/src/App.js
+++ 2-set-and-update-state-with-useState/state-hook/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   const [ score, setScore ] = useState(0);
+  const [ message, setMessage ] = useState('Welcome!');
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{ message }</h1>
         <h1>{ score }</h1>
         <button onClick={() => setScore(score + 1)}>
           Increase score
