4868536b65510a00c0cb1b008fe429e29c727c9b
Nico Rithner <nico@altvia.com>
2021-07-28 07:40:33 -0600

    Set and update state

--- 2-set-and-update-state-with-useState/state-hook/src/App.js
+++ 2-set-and-update-state-with-useState/state-hook/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ score, setScore ] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome!</h1>
+        <h1>{ score }</h1>
+        <button onClick={() => setScore(score + 1)}>
+          Increase score
+        </button>
       </header>
     </div>
   );
